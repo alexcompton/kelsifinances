3fc2b13121be55e15d62451c7769a3aa6c2a4548
alexcompton <alexcompton11@gmail.com>
2016-09-18 16:08:18 -0400

    component change

--- client/app/components/home/HomeComponent.ts
+++ client/app/components/home/HomeComponent.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from 'angular2/core';
 
 @Component({
     selector: 'home',
-    template: `<h1>Home</h1>`
+    template: `<h1>Kelsi's finance app</h1>`
 })
 
 export class HomeComponent implements OnInit {
